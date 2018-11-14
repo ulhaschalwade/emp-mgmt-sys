fcdd2191d2481025dacf8784b78c4eccc58af466
Ulhas Chalawade <ulhas.chalawade@globant.com>
2018-11-14 16:10:27 +0530

    Use const instead of let wherever required

--- service/organisation.js
+++ service/organisation.js
@@ -4,38 +4,38 @@ const logger = global['logger'];
 class OrganisationService {
     async getAllOrganisations() {
         logger.debug('Request for get all organisations received..');
-        let organisations = await orgModel.find();
+        const organisations = await orgModel.find();
         return organisations;
     }
 
     async getOrganisationById(id) {
         logger.debug('Request for get organisation by id received..');
-        let organisation = await orgModel.findById(id);
+        const organisation = await orgModel.findById(id);
         return organisation;
     }
 
     async updateOrganisationById(id, updatedObj) {
         logger.debug('Request for update organisation by id received..');
-        let updatedOrganisation = await orgModel.findByIdAndUpdate(id
+        const updatedOrganisation = await orgModel.findByIdAndUpdate(id
             , updatedObj);
         return updatedOrganisation;
     }
 
     async deleteOrganisationById(id) {
         logger.debug('Request for delete organisation by id received..');
-        let deletedOrganisation = await orgModel.findByIdAndRemove(id);
+        const deletedOrganisation = await orgModel.findByIdAndRemove(id);
         return deletedOrganisation;
     }
 
     async addNewOrganisation(newObj) {
         logger.debug('Request for add new organisation received..');
-        let newOrganisation = await orgModel.create(newObj);
+        const newOrganisation = await orgModel.create(newObj);
         return newOrganisation;
     }
 
     async getAllEmployeesFromOrganisation(orgId) {
         logger.debug('Request for get all employees of organisation received..');
-        let employees = await orgModel.findById(orgId)
+        const employees = await orgModel.findById(orgId)
             .populate('Employee', ['firstName', 'lastName'])
             .exec()
         return employees;
@@ -43,7 +43,7 @@ class OrganisationService {
 
     async deleteAllEmployeesFromOrganisation(orgId) {
         logger.debug('Request for delete all employees of organisation received..');
-        let result = orgModel.findById(orgId)
+        const result = orgModel.findById(orgId)
             .populate('Employee')
             .exec((error, employees) => {
                 if (error)
@@ -56,7 +56,7 @@ class OrganisationService {
 
     async getEmployeeFromOrganisation(orgId, empId) {
         logger.debug('Request for get employee by id of an organisation received..');
-        let employee = await orgModel.findById(orgId)
+        const employee = await orgModel.findById(orgId)
             .populate('Employee', ['firstName', 'lastName'], null, { _id: empId })
             .exec()
         return employee;
@@ -64,7 +64,7 @@ class OrganisationService {
 
     async removeEmployeeFromOrganisation(orgId, empId) {
         logger.debug('Request for remove employee by id of an organisation received..');
-        let result = orgModel.findById(orgId)
+        const result = orgModel.findById(orgId)
             .populate('Employee')
             .exec((error, employees) => {
                 if (error)
@@ -79,7 +79,7 @@ class OrganisationService {
 
     async addEmployeeInOrganisation(orgId, empId) {
         logger.debug('Request for add new employee in organisation received..');
-        let result = orgModel.findById(orgId)
+        const result = orgModel.findById(orgId)
             .populate('Employee')
             .exec((error, employees) => {
                 if (error)
